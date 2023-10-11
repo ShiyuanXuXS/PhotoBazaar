68bf7a44bd5cf2119c9adfcea0387353294e141a
xiaoxing pan <124628809+xiaoxingpan@users.noreply.github.com>
2023-10-11 09:53:12 -0400

    update header

---
+++ client/src/pages/ArtworkList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function ArtworkList() {
+
+    return (
+        <div className='text-5xl'>This is your artwork page!</div>
+    )
+}
+
+export default ArtworkList;

084d5002918919485138c3b56fb174abb522968d
ShiyuanXuXS <shiyuan.xu.xs@gmail.com>
2023-10-15 17:44:46 -0400

    payment modified

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
 import axios from "axios";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import PayForm from '../components/PayForm'
 
-import CheckoutForm from "../components/CheckoutForm";
-console.log(process.env);
-const stripeKey = process.env.REACT_APP_STRIPE_API_KEY;
-const stripePromise = loadStripe(stripeKey);
 
 function Payment() {
     const purchase_id="652226c2f905b8dea4f77130"
